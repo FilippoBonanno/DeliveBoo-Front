a07fab5e1dd240a1165b5f807f702b88a753e27d
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-09 11:59:25 +0200

    getPrice da sistemare

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -82,7 +82,7 @@ export default {
                             <div @click="handleRemoveFromCart(item.id)"><small
                                     class="text-muted mt-1 btn btn-danger">Elimina</small></div>
                         </div>
-                        <span class="text-muted">€{{ getPrice(item.price) }}</span>
+                        <span class="text-muted">€{{ item.price }}</span>
                     </li>
                 </ul>
                 <div v-if="getCart().length !== 0" @click="handleClearCart()"><small
