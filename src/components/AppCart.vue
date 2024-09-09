9443fd502d5b659b0db505def6f1b676601e0ddf
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-09 11:57:11 +0200

    fixGetPrice

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -37,6 +37,9 @@ export default {
                 console.log(this.cart[i].price, sum);
             }
             return sum.toFixed(2);
+        },
+        getPrice(price) {
+            return price.toFixed(2);
         }
 
     },
@@ -79,7 +82,7 @@ export default {
                             <div @click="handleRemoveFromCart(item.id)"><small
                                     class="text-muted mt-1 btn btn-danger">Elimina</small></div>
                         </div>
-                        <span class="text-muted">€{{ item.price.toFixed(2) }}</span>
+                        <span class="text-muted">€{{ getPrice(item.price) }}</span>
                     </li>
                 </ul>
                 <div v-if="getCart().length !== 0" @click="handleClearCart()"><small
