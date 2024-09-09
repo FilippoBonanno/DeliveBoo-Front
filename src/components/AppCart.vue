ae225b215601f46b0786c407533244684d9b2a88
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-09 12:03:02 +0200

    fix items price

--- src/components/AppCart.vue
+++ src/components/AppCart.vue
@@ -82,7 +82,7 @@ export default {
                             <div @click="handleRemoveFromCart(item.id)"><small
                                     class="text-muted mt-1 btn btn-danger">Elimina</small></div>
                         </div>
-                        <span class="text-muted">€{{ item.price }}</span>
+                        <span class="text-muted">€{{ parseFloat(item.price).toFixed(2) }}</span>
                     </li>
                 </ul>
                 <div v-if="getCart().length !== 0" @click="handleClearCart()"><small
