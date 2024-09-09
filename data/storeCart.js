f64940cd4ffa58745aba1d42566dba655d29f100
EdoardoMenicucci <edoardo.menicucci@libero.it>
2024-09-09 10:21:54 +0200

    store function / data cart

--- data/storeCart.js
+++ data/storeCart.js
@@ -2,5 +2,25 @@
 import { reactive } from "vue";
 
 export const store = reactive({
-  count: 0,
+  //Variabili
+  cart: JSON.parse(localStorage.getItem("cart")) || [],
+
+  //Funzioni
+  addToCart(product) {
+    this.cart.push(product);
+    localStorage.setItem("cart", JSON.stringify(this.cart));
+  },
+  removeFromCart(productId) {
+    this.cart = this.cart.filter((item) => item.id !== productId);
+    localStorage.setItem("cart", JSON.stringify(this.cart));
+  },
+  clearCart() {
+    this.cart = [];
+    localStorage.setItem("cart", JSON.stringify(this.cart));
+  },
+
+  //Getter
+  getCart() {
+    return this.cart;
+  },
 });
