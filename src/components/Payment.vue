6a510fed54464d0c34c08b5f0d55c1c8d9ba1fc1
MikeDesktop <micheledong2803@gmail.com>
2024-09-09 14:18:02 +0200

    italiano

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -15,7 +15,7 @@ export default {
     ,
     mounted() {
         axios.get('http://localhost:8000/api/token').then(response => {
-            
+
             this.token = response.data;
 
             if (this.token && this.token.token) {
@@ -23,7 +23,8 @@ export default {
                 braintree.dropin.create({
                     container: '#dropin-container',
                     authorization: this.token.token,
-                    dataCollector:true
+                    dataCollector: true,
+                    locale: 'it_IT'
                 },
                     (error, dropinInstance) => {
                         if (error) {
@@ -47,7 +48,7 @@ export default {
                                 document.getElementById('device').value = payload.deviceData; //non usare il binding perché braintree ha un ciclo di vita esterno a vue
 
                                 // Nonce
-                                document.getElementById('nonce').value = payload.nonce; 
+                                document.getElementById('nonce').value = payload.nonce;
 
                                 // Form submit
                                 this.$refs.form.submit();
@@ -66,10 +67,80 @@ export default {
 
     <form id="payment-form" action="http://localhost:8000/api/checkout" method="post" ref="form">
 
-        <div id="dropin-container" class="w-50 m-auto"></div>
-        <input type="submit" />
-        <input type="hidden" id="nonce" name="payment_method_nonce" />
-        <input name="device_data" type="hidden" id="device">
+        <div class="container">
+            <div class="col-md-6 offset-md-3">
+                <h1>Checkout</h1>
+                <div class="form-group">
+                    <label for="email">Indirizzo Email</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+
+                <div class="form-group">
+                    <label for="name_on_card">Nome e Cognome</label>
+                    <input type="text" class="form-control" id="name_on_card" name="name_on_card">
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="address">Indirizzo</label>
+                            <input type="text" class="form-control" id="address" name="address">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="city">Città</label>
+                            <input type="text" class="form-control" id="city" name="city">
+                        </div>
+                    </div>
+
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="province">Provincia</label>
+                            <input type="text" class="form-control" id="province" name="province">
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="postalcode">Codice Postale</label>
+                            <input type="text" class="form-control" id="postalcode" name="postalcode">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="country">Paese</label>
+                            <input type="text" class="form-control" id="country" name="country">
+                        </div>
+                    </div>
+
+                    <div class="col-md-4">
+                        <div class="form-group">
+                            <label for="phone">Telefono</label>
+                            <input type="text" class="form-control" id="phone" name="phone">
+                        </div>
+                    </div>
+
+                    <div class="col-md-12">
+                        <div class="form-group">
+                            <div id="dropin-container"></div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <button type="submit" class="btn btn-success">Acquista ora</button>
+
+                <input type="hidden" id="nonce" name="payment_method_nonce" />
+                <input name="device_data" type="hidden" id="device">
+            </div>
+        </div>
+
     </form>
 
 </template>
