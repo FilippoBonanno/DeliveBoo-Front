2072da3befe62ce7926e23b0f2b8415aff678705
MikeDesktop <micheledong2803@gmail.com>
2024-09-09 13:57:46 +0200

    device data

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -8,7 +8,6 @@ export default {
         return {
             token: null,
             dropinInstance: null,
-            nonceValue: null,
         }
     },
     methods: {
@@ -16,12 +15,15 @@ export default {
     ,
     mounted() {
         axios.get('http://localhost:8000/api/token').then(response => {
+            
             this.token = response.data;
+
             if (this.token && this.token.token) {
 
                 braintree.dropin.create({
                     container: '#dropin-container',
                     authorization: this.token.token,
+                    dataCollector:true
                 },
                     (error, dropinInstance) => {
                         if (error) {
@@ -29,17 +31,25 @@ export default {
                             return;
                         }
 
+                        // Al submit del form...
                         this.$refs.form.addEventListener('submit', (event) => {
+                            // Previeni il submit se ci sono errori
                             event.preventDefault();
 
+                            // Richiedi il metodo di pagamento
                             dropinInstance.requestPaymentMethod((error, payload) => {
+                                // Interrompi se ci sono errori
                                 if (error) {
                                     console.error(error);
                                     return;
                                 }
+                                // Device Data
+                                document.getElementById('device').value = payload.deviceData; //non usare il binding perché braintree ha un ciclo di vita esterno a vue
 
-                                document.getElementById('nonce').value = payload.nonce; //non usare il binding perché braintree ha un ciclo di vita esterno a vue
+                                // Nonce
+                                document.getElementById('nonce').value = payload.nonce; 
 
+                                // Form submit
                                 this.$refs.form.submit();
                             });
                         });
@@ -56,9 +66,10 @@ export default {
 
     <form id="payment-form" action="http://localhost:8000/api/checkout" method="post" ref="form">
 
-        <div id="dropin-container"></div>
+        <div id="dropin-container" class="w-50 m-auto"></div>
         <input type="submit" />
         <input type="hidden" id="nonce" name="payment_method_nonce" />
+        <input name="device_data" type="hidden" id="device">
     </form>
 
 </template>
